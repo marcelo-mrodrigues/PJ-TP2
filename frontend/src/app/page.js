9138f86bb2384c597bcf07bd473e819b936359da
Nev0 <nevenarcisista@gmail.com>
2025-07-13 22:37:56 -0300

    display current user info

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -12,8 +12,28 @@ import { Pagination } from 'swiper/modules';
 // 3. Importar o CSS do Swiper
 import 'swiper/css';
 import 'swiper/css/pagination'; // Se você usar paginação
+import { useEffect } from "react";
+
+async function get_user_details() {
+  
+}
 
 export default function Home() {
+  useEffect(() => {
+    const fetchUserDetails = async() => {
+      const response = await fetch("http://localhost:8000/api/v1/me", {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Accept': 'application/json',
+        }
+      })
+      const data = await response.json()
+      console.log(data)
+    }
+
+    fetchUserDetails();
+  }, []) 
   return (
     <section>
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
@@ -122,53 +142,6 @@ export default function Home() {
             </div>
           </section>
 
-          {/* CÓDIGO ORIGINAL DO NEXT.JS (APENAS A SEÇÃO PRINCIPAL) */}
-          <Image
-            className="dark:invert"
-            src="/next.svg"
-            alt="Next.js logo"
-            width={180}
-            height={38}
-            priority
-          />
-          <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-            <li className="mb-2 tracking-[-.01em]">
-              Get started by editing{" "}
-              <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                src/app/page.js
-              </code>
-              .
-            </li>
-            <li className="tracking-[-.01em]">
-              Save and see your changes instantly.
-            </li>
-          </ol>
-
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read our docs
-            </a>
-          </div>
         </main>
         {/* Adicionar o footer */}
         <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
